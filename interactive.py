1ead0881dcc76a5cab5c183f00c9aa9bbc598dd8
Yana <155889900+ypapish@users.noreply.github.com>
2025-03-12 19:03:51 +0200

    add function to get numbers

--- interactive.py
+++ interactive.py
@@ -1,1 +1,11 @@
+from calculation import calculate_roots
+
+
+def get_numbers(prompt):
+    while True:
+        value = input(prompt).strip()
+        try:
+            return float(value)
+        except ValueError:
+            print(f"Error: expected a valid real number, got {value} instead")
 
