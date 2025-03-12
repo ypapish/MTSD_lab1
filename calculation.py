906c4fe6e4eddeadd70620fea1b820e96af594d0
Yana <y.papish14@gmail.com>
2025-03-12 19:12:39 +0200

    change calculation

--- calculation.py
+++ calculation.py
@@ -2,10 +2,10 @@ def calculate_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         print("Discriminant < 0. There are no real roots")
-    elif discriminant == 0:
-        x = -b / (2 * a)
-        print(f"Discriminant = 0. There is one root: x = {x}")
     else:
         x1 = (-b - discriminant**0.5) / (2 * a)
         x2 = (-b + discriminant**0.5) / (2 * a)
-        print(f"Discriminant > 0. There are two roots: x1 = {x1}, x2 = {x2}")
+        if discriminant == 0:
+            print(f"Discriminant = 0. There is one root: x = {x1}")
+        else:
+            print(f"Discriminant > 0. There are two roots: x1 = {x1}, x2 = {x2}")
