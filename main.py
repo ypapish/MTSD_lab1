c70e3752940fa149d8054b208eaf8a06c7525132
Yana <155889900+ypapish@users.noreply.github.com>
2025-03-12 19:07:53 +0200

    add main function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import sys
+from interactive import interactive_mode
+from noninteractive import noninteractive_mode
 
+
+def main():
+    if len(sys.argv) == 2:
+        noninteractive_mode()
+    else:
+        interactive_mode()
+
+
+if __name__ == "__main__":
+    main()
