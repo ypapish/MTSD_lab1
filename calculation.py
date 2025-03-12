9404cd85f540acb1098abd99ced531a37b5e7c9e
Yana <155889900+ypapish@users.noreply.github.com>
2025-03-12 21:32:08 +0200

    improve calculations

--- calculation.py
+++ calculation.py
@@ -1,3 +1,6 @@
+import math
+
+
 def calculate_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
@@ -6,6 +9,6 @@ def calculate_roots(a, b, c):
         x = -b / (2 * a)
         print(f"Discriminant = 0. There is one root: x = {x}")
     else:
-        x1 = (-b - discriminant**0.5) / (2 * a)
-        x2 = (-b + discriminant**0.5) / (2 * a)
+        x1 = (-b - math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b + math.sqrt(discriminant)) / (2 * a)
         print(f"Discriminant > 0. There are two roots: x1 = {x1}, x2 = {x2}")
